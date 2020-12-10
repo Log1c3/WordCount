d098b3c8bfdb665a9e652bfbf854844b615efb2d
Log1c3 <log1c3@qq.com>
2020-12-10 12:23:29 +0800

    版本更新

--- py_case/WordCount.py
+++ py_case/WordCount.py
@@ -7,38 +7,49 @@ def CountChar(file):
     space = 0
     digit = 0
     others = 0
-    file = sys.argv[1]
     fp = open(file, 'r')
     string = fp.read()
     print("内容：" + string)
     fp.close()
     for c in string:
-        if c.isalpha():
+        if c.isalpha():#判断字母
             letters += 1
-        elif c.isdigit():
+        elif c.isdigit():#判断数字
             digit += 1
-        elif c.isspace():
+        elif c.isspace():#判断空格
             space += 1
-        else:
+        else:#其他
             others += 1
-    print("文件中有%d个英文字母，%d个数字，%d个空格，%d个其他字符" % (letters, digit, space, others))
+    print("%d个英文字母\n%d个数字\n%d个空格\n%d个其他字符" % (letters, digit, space, others))
 
 
 def CountWord(file):
-    file = sys.argv[1]
-    pattern = r',|\.|/|;|\'|`|\[|\]|<|>|\?|:|"|\{|\}|\~|!|@|#|\$|%|\^|&|\(|\)|-|=|\_|\+|，|。|、|；|‘|’|【|】|·|！| |…|（|）'
+    patter = r',|\.|/|;|\'|`|\[|\]|<|>|\?|:|"|\{|\}|\~|!|@|#|\$|%|\^|&|\(|\)|-|=|\_|\+|，|。|、|；|‘|’|【|】|·|！| |…|（|）'
+    #所有标点符我把它看作隔板
     fp = open(file, 'r')
     string = fp.read()
-    print("内容：\n" + string)
+    print("内容：" + string)
+    #输出读取的文本
     fp.close()
-    ls = re.split(pattern, string)
-    print("单词数=%d" % --len(ls))
+    ls = re.split(patter, string)
+    #隔板将string分隔并存储在ls中
+    length = len(ls)
+    if string[len(string)-1] in patter:
+    #如果在string的结尾或开头能匹配pattern
+    # 则会在返回列表的最后一个元素或第一个元素是空字符串，所以要-1
+        length -= 1
+        print("单词数=", length)
+    else:
+        print("单词数=", length)
 
 
 print("脚本名：", sys.argv[0])
 for i in range(1, len(sys.argv)):
-    print("参数：", i, sys.argv[i])
-if sys.argv[2] == "-c" or sys.argv[2] == "-C":
-    CountChar(sys.argv[1])
-elif sys.argv[2] == "-w" or sys.argv[2] == "-W":
-    CountWord(sys.argv[1])
+    if(i == 1):
+        print("指令：\t", sys.argv[i])
+    if(i == 2):
+        print("文件名：", sys.argv[i])
+if sys.argv[1] == "-c" or sys.argv[1] == "-C":
+    CountChar(sys.argv[2])
+elif sys.argv[1] == "-w" or sys.argv[1] == "-W":
+    CountWord(sys.argv[2])
